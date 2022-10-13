9fdd8151017ac0bf3e84c32d7fcb84d4b125b475
Yoooum <akyomi@qq.com>
2022-10-13 09:27:39 +0800

    更新：由于重写数据库表，更新POJO类

--- src/main/java/com/prprv/shop/pojo/OrderGoods.java
+++ src/main/java/com/prprv/shop/pojo/OrderGoods.java
@@ -1,85 +1,73 @@
 package com.prprv.shop.pojo;
 
-/**
- * 订单商品信息
- */
+
 public class OrderGoods {
-    private Integer id;//订单商品id
-    private Integer oid;//订单id
-    private Integer gid;//商品id
-    private Integer uid;//用户id
-    private Integer amount;//商品数量
-    private Double totalPrice;//商品总价
 
-    public OrderGoods() {
-    }
+  private Integer id;
+  private Integer uid;
+  private Integer oid;
+  private Integer amount;
+  private double price;
 
-    public OrderGoods(Integer id, Integer oid, Integer gid, Integer uid, Integer amount, Double totalPrice) {
-        this.id = id;
-        this.oid = oid;
-        this.gid = gid;
-        this.uid = uid;
-        this.amount = amount;
-        this.totalPrice = totalPrice;
-    }
+  public OrderGoods() {
+  }
 
-    public Integer getId() {
-        return id;
-    }
+  public OrderGoods(Integer id, Integer uid, Integer oid, Integer amount, double price) {
+    this.id = id;
+    this.uid = uid;
+    this.oid = oid;
+    this.amount = amount;
+    this.price = price;
+  }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+  public Integer getId() {
+    return id;
+  }
 
-    public Integer getOid() {
-        return oid;
-    }
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-    public void setOid(Integer oid) {
-        this.oid = oid;
-    }
+  public Integer getUid() {
+    return uid;
+  }
 
-    public Integer getGid() {
-        return gid;
-    }
+  public void setUid(Integer uid) {
+    this.uid = uid;
+  }
 
-    public void setGid(Integer gid) {
-        this.gid = gid;
-    }
+  public Integer getOid() {
+    return oid;
+  }
 
-    public Integer getUid() {
-        return uid;
-    }
+  public void setOid(Integer oid) {
+    this.oid = oid;
+  }
 
-    public void setUid(Integer uid) {
-        this.uid = uid;
-    }
+  public Integer getAmount() {
+    return amount;
+  }
 
-    public Integer getAmount() {
-        return amount;
-    }
+  public void setAmount(Integer amount) {
+    this.amount = amount;
+  }
 
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
+  public double getPrice() {
+    return price;
+  }
 
-    public Double getTotalPrice() {
-        return totalPrice;
-    }
+  public void setPrice(double price) {
+    this.price = price;
+  }
 
-    public void setTotalPrice(Double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    @Override
-    public String toString() {
-        return "OrderGoods{" +
-                "id=" + id +
-                ", oid=" + oid +
-                ", gid=" + gid +
-                ", uid=" + uid +
-                ", amount=" + amount +
-                ", totalPrice=" + totalPrice +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "OrderGoods{" +
+            "id=" + id +
+            ", uid=" + uid +
+            ", oid=" + oid +
+            ", amount=" + amount +
+            ", price=" + price +
+            '}';
+  }
 }
